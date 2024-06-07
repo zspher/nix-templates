13069d5013e2ae33108872d6448d7e3e7f4fa6b4
zspher <66728045+zspher@users.noreply.github.com>
2024-06-07 18:12:58 +0800

    feat(flake): remove descriptions

--- flake.nix
+++ flake.nix
@@ -2,17 +2,14 @@
   description = "a collection of templates";
   outputs = {self}: {
     templates = {
-        description = "c example";
       c-cpp = {
         path = ./c-cpp;
       };
       rust-example = {
         path = ./rust-example;
-        description = "rust example";
       };
       c-sharp-example = {
         path = ./c-sharp-example;
-        description = "c# example";
       };
     };
   };
