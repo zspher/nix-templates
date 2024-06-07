af224ba0aa36b0751dcb151f2ee31d08392b8534
zspher <66728045+zspher@users.noreply.github.com>
2024-06-07 19:03:49 +0800

    feat: add template of templates

---
+++ main-template/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = [
+      "aarch64-linux"
+      "x86_64-linux"
+    ];
+    perSystem = f:
+      nixpkgs.lib.genAttrs systems (system: f {pkgs = nixpkgs.legacyPackages.${system};});
+  in {
+    devShells = perSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+        ];
+
+        nativeBuildInputs = with pkgs; [];
+        buildInputs = with pkgs; [];
+      };
+    });
+  };
+}
