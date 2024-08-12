77ca0a16e61830ef3df08e8e6d734230d5ca7bc5
zspher <66728045+zspher@users.noreply.github.com>
2024-08-12 14:29:37 +0800

    feat: add latex template

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
     templates = {
       c-cpp = {path = ./c-cpp;};
       c-sharp = {path = ./c-sharp;};
+      latex = {path = ./latex;};
       notebook = {path = ./notebook;};
       python = {path = ./python;};
       rust = {path = ./rust;};
