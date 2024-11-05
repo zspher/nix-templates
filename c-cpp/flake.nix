891e5a6ea2ee19408aff94d7d98c39ca0656bbd6
zspher <66728045+zspher@users.noreply.github.com>
2024-11-04 19:38:55 -0700

    feat: revamp c-cpp template

--- c-cpp/flake.nix
+++ c-cpp/flake.nix
@@ -2,26 +2,47 @@
   description = "example c/cpp program";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = {nixpkgs, ...}: let
-    systems = [
-      "aarch64-linux"
-      "x86_64-linux"
-    ];
-    perSystem = f:
-      nixpkgs.lib.genAttrs systems (system: f {pkgs = nixpkgs.legacyPackages.${system};});
-  in {
-    devShells = perSystem ({pkgs}: {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          clang-tools # LSP, formatter
-        ];
+  outputs =
+    { nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+      perSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+          }
+        );
+    in
+    {
+      devShells = perSystem (
+        { pkgs }:
+        {
+          default =
+            pkgs.mkShell.override
+              {
+                # Override stdenv in order to change compiler:
+                # stdenv = pkgs.clangStdenv;
+              }
+              {
+                packages = with pkgs; [
+                  clang-tools # LSP, formatter static analyser
 
-        nativeBuildInputs = with pkgs; [
-          meson
-          ninja
-          pkg-config
-        ];
-      };
-    });
-  };
+                  meson
+                  ninja
+                  pkg-config
+
+                  gtest
+                  lcov
+                ];
+              };
+        }
+      );
+    };
 }
