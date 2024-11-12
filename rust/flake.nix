fc59b9b1b5071ca4919afa234d1987b5cd579ca0
zspher <66728045+zspher@users.noreply.github.com>
2024-11-12 11:42:49 -0700

    feat: revamp rust template

--- rust/flake.nix
+++ rust/flake.nix
@@ -5,31 +5,72 @@
   inputs.rust-overlay.url = "github:oxalica/rust-overlay";
   inputs.rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = {
-    self,
-    nixpkgs,
-    rust-overlay,
-  }: let
-    systems = [
-      "aarch64-linux"
-      "x86_64-linux"
-    ];
-    perSystem = f:
-      nixpkgs.lib.genAttrs systems (system:
-        f {
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [(import rust-overlay)];
-          };
-        });
-  in {
-    devShells = perSystem ({pkgs}: {
-      default = pkgs.mkShell {
-        packages = with pkgs; [];
-        nativeBuildInputs = [
-          pkgs.rust-bin.stable.latest.default
-        ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+    }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        f:
+        nixpkgs.lib.genAttrs systems (
+          system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [
+                rust-overlay.overlays.default
+                self.overlays.default
+              ];
+            };
+          }
+        );
+    in
+    {
+      overlays.default = final: prev: {
+        rustToolchain =
+          let
+            rust = prev.rust-bin;
+          in
+          if builtins.pathExists ./rust-toolchain.toml then
+            rust.fromRustupToolchainFile ./rust-toolchain.toml
+          else if builtins.pathExists ./rust-toolchain then
+            rust.fromRustupToolchainFile ./rust-toolchain
+          else
+            rust.stable.latest.default.override {
+              extensions = [
+                "rust-src"
+                "rustfmt"
+              ];
+            };
       };
-    });
-  };
+      devShells = perSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              rustToolchain
+              openssl
+              pkg-config
+              cargo-deny
+              cargo-edit
+              cargo-watch
+              rust-analyzer
+            ];
+
+            shellHook = ''
+              # Required by rust-analyzer
+              export RUST_SRC_PATH=${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+            '';
+          };
+        }
+      );
+    };
 }
