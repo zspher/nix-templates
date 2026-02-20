5c7c17fce5504c90ec577b5a49c188e92b476ff4
zspher <66728045+zspher@users.noreply.github.com>
2026-02-20 00:45:29 -0700

    feat(rust): update template
    - use fenix
    - cargo-watch -> bacon

--- rust/flake.nix
+++ rust/flake.nix
@@ -1,74 +1,54 @@
 {
-  description = "example rust program";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
-  inputs.rust-overlay.url = "github:oxalica/rust-overlay";
-  inputs.rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      rust-overlay,
-    }:
+    { nixpkgs, fenix, ... }:
     let
-      systems = [
+      inherit (nixpkgs) lib;
+      perSystem = lib.genAttrs [
         "x86_64-linux"
         "aarch64-linux"
         "x86_64-darwin"
         "aarch64-darwin"
       ];
-      perSystem =
-        f:
-        nixpkgs.lib.genAttrs systems (
-          system:
-          f {
-            pkgs = import nixpkgs {
-              inherit system;
-              overlays = [
-                rust-overlay.overlays.default
-                self.overlays.default
-              ];
-            };
-          }
-        );
     in
     {
-      overlays.default = final: prev: {
-        rustToolchain =
-          let
-            rust = prev.rust-bin;
-          in
-          if builtins.pathExists ./rust-toolchain.toml then
-            rust.fromRustupToolchainFile ./rust-toolchain.toml
-          else if builtins.pathExists ./rust-toolchain then
-            rust.fromRustupToolchainFile ./rust-toolchain
-          else
-            rust.stable.latest.default.override {
-              extensions = [
-                "rust-src"
-                "rustfmt"
-              ];
-            };
-      };
       devShells = perSystem (
-        { pkgs }:
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          rustToolchain =
+            with fenix.packages.${pkgs.stdenv.hostPlatform.system};
+            combine (
+              with stable;
+              [
+                clippy
+                rustc
+                cargo
+                rustfmt
+                rust-src
+              ]
+            );
+        in
         {
           default = pkgs.mkShell {
             packages = with pkgs; [
               rustToolchain
               openssl
               pkg-config
+
               cargo-deny
-              cargo-edit
-              cargo-watch
+              bacon
               rust-analyzer
             ];
 
-            shellHook = ''
-              # Required by rust-analyzer
-              export RUST_SRC_PATH=${pkgs.rustToolchain}/lib/rustlib/src/rust/library
-            '';
+            env.RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
           };
         }
       );
