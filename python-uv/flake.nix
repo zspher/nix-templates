c11fdaa4b292efa92306942d277bf3e312ad5014
zspher <66728045+zspher@users.noreply.github.com>
2026-02-21 21:26:31 -0700

    feat(python-uv): prevent creating .venv
    - uv2nix handles deps

--- python-uv/flake.nix
+++ python-uv/flake.nix
@@ -84,11 +84,11 @@
               UV_NO_SYNC = "1";
               UV_PYTHON = pythonSet.python.interpreter;
               UV_PYTHON_DOWNLOADS = "never";
+              UV_PROJECT_ENVIRONMENT = pythonEnv; # prevent creating .venv
             };
             shellHook = ''
               unset PYTHONPATH
               export REPO_ROOT=$(git rev-parse --show-toplevel)
-              uv lock
             '';
           };
         }
